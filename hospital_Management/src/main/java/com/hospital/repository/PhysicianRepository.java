657730ab739557409a828e3236e1b168d0d8d3fb
HumbleTyrant <the.tyrant152601@gmail.com>
2026-05-16 01:38:48 +0530

    Adding Physician controller and updating it's repo

--- hospital_Management/src/main/java/com/hospital/repository/PhysicianRepository.java
+++ hospital_Management/src/main/java/com/hospital/repository/PhysicianRepository.java
@@ -1,6 +1,9 @@
 package com.hospital.repository;
 
 import com.hospital.entity.Physician;
+import com.hospital.projection.PhysicianProjection;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
@@ -9,16 +12,14 @@ import java.util.Optional;
 
 @RepositoryRestResource(path = "physicians")
 public interface PhysicianRepository extends JpaRepository<Physician, Integer> {
-
+    Page<PhysicianProjection> findAllProjectedBy(Pageable pageable);
+    Optional<PhysicianProjection> findProjectedByEmployeeId(Integer employeeId);
+    Page<PhysicianProjection> findByNameContainingIgnoreCase(String name, Pageable pageable);
     Optional<Physician> findByName(String name);
-
     List<Physician> findByNameContainingIgnoreCase(String namePart);
-
     List<Physician> findByPosition(String position);
-
     List<Physician> findByPositionContainingIgnoreCase(String keyword);
-
     Optional<Physician> findBySsn(Integer ssn);
     boolean existsBySsn(Integer ssn);
-
+    List<Physician> findAllByOrderByNameAsc();
 }
